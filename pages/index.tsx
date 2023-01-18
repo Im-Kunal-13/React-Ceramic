c30aa1e68782d746c95f561e47a903e2a1426122
ImKunal13z <2002kunalmondal13@gmail.com>
2023-01-18 07:32:08 +0530

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,9 @@
 import type { NextPage } from "next"
 import * as React from "react"
-import { ChakraProvider, Box, theme } from "@chakra-ui/react"
-import TopBar from "../components/TopBar"
-import TodoList from "../components/TodoList"
-import { NoSsr } from "@mui/base"
-import TodoAdd from "../components/TodoAdd"
+
 
 const Home: NextPage = () => {
-    return (
-        <ChakraProvider theme={theme}>
-            <Box maxWidth="8xl" margin="auto" p={5}>
-                <TopBar />
-                <NoSsr>
-                    <TodoList />
-                </NoSsr>
-                <TodoAdd />
-            </Box>
-        </ChakraProvider>
-    )
+    return <div>Yo 😁😁😁</div>
 }
 
 export default Home
